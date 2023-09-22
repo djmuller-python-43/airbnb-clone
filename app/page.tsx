7083c798084bef69a832b563e5a9ab3371864120
duane <djmuller.python.43@gmail.com>
2023-09-22 15:16:14 +0200

    create emptystate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import Container from './(shared)/Container';
+import EmptyState from './(shared)/EmptyState';
 
 
 export default function Home() {
+  const isEmpty = true
+
+  if(isEmpty) {
+    return (
+      <EmptyState 
+        showReset
+      />
+    )
+  }
   return (
-    <div><h1 className="text-3xl text-black">Hello World</h1></div>
+    <Container>
+      <div 
+        className='
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl;grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+      '>
+        <div>
+          my future listings
+        </div>
+      </div>
+    </Container>
   )
 }
